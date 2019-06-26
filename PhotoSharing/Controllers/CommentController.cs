f1c664a46d7d81b2d783e8a6b98c8c079891327f
謝光欣 <robinkhh@gmail.com>
2019-06-26 15:50:50 +0800

    加入專案檔案。

---
+++ PhotoSharing/Controllers/CommentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhotoSharing.DAL;
+using PhotoSharing.Models;
+
+
+namespace PhotoSharing.Controllers
+{
+    public class CommentController : Controller
+    {
+        PhotoSharingContext context = new PhotoSharingContext();
+
+        // GET: Comment
+        [ChildActionOnly]
+        public ActionResult _CommentsForPhoto(int PhotoID)
+        {
+            var comments = from c in context.Comments
+                           where c.PhotoID == PhotoID
+                           select c;
+            ViewBag.PhotoID = PhotoID;
+
+            return PartialView(comments.ToList());
+        }
+    }
+}
